324236a368629cf24d919851804f61d43ca1eeac
Houbar231 <matousfiala@gmail.com>
2016-08-23 13:52:49 +0200

    Added the NameGen
    NameGen.Group() a NameGen.Person(string profession, int format(0-1))

---
+++ Assets/Scripts/UtilityScripts/GeneralUtilies.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Rand {
+    public static T Array<T>(T[] array) {
+        return array[Random.Range(0, array.Length-1)];
+    }
+}
---
+++ Assets/Scripts/UtilityScripts/UtilityScriptsReference.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UtilityScriptsReference : MonoBehaviour {
+    public static UtilityScriptsReference r; //For easy acces
+    public string SBefore;
+    public string SAfter;
+    public string SNouns;
+
+    public string[] Before;
+    public string[] After;
+    public string[] Nouns;
+
+    void Awake() {
+        r = this;
+        Before = SBefore.Split(',');
+        After = SAfter.Split(',');
+        Nouns = SNouns.Split(',');
+
+        for(int i = 0; i < 100; i++) {
+            Debug.Log(NameGen.Group());
+        }
+    }
+}
