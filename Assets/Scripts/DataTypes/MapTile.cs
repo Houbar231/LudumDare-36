674994f6864332194aa721ea92e2c967ecad9136
Houbar231 <matousfiala@gmail.com>
2016-08-27 07:33:46 +0200

    Added mouse support

--- Assets/Scripts/DataTypes/MapTile.cs
+++ Assets/Scripts/DataTypes/MapTile.cs
@@ -8,9 +8,7 @@ public class MapTile {
     public MapTile(int x, int y) {
         Data = new TileData(x, y);
         TileGO = new GameObject("Tile " + x + ", " + y);
-        TileGO.transform.position = new Vector3(x, 0, y) + new Vector3(-(GeneralReference.r.Width / 2f), 0, -(GeneralReference.r.Height / 2f))
-
-
+        TileGO.transform.position = new Vector3(x, y);// + new Vector3(-(GeneralReference.r.Width / 2f), 0, -(GeneralReference.r.Height / 2f))
         TileGO.AddComponent<SpriteRenderer>().sprite = SpriteReference.r.DefaultTileSprite;
     }
 }
