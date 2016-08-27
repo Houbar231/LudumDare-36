2fe021afae131f8ef21dea4eb59301c96d109bae
GLProgrammer <jirka2002@gmail.com>
2016-08-27 10:36:17 +0200

    BigFix
    Fix of founding tiles

--- Assets/Scripts/Cams/CameraMove.cs
+++ Assets/Scripts/Cams/CameraMove.cs
@@ -3,34 +3,11 @@ using System.Collections;
 
 public class CameraMove : MonoBehaviour {
     private Vector3 movement;
- -    public Camera MainCamera;
- -    public bool Arrows = true;
- -    public float speed = 1.0f;
- -    private float movementX, movementY;
-    public float speed = 1;
+     public Camera MainCamera;
+     public bool Arrows = true;
+    public float speed = 1.0f;
+     private float movementX, movementY;
 	
-	// Update is called once per frame
-	void Update () {
-        if (Arrows)
-        {
-                movementX = Input.GetAxisRaw("Vertical") * speed;
-                movementY = Input.GetAxisRaw("Horizontal") * speed;
-                MainCamera.transform.Translate(movementY, movementX, 0);
-        }
-        else
-        {
-
-                movementX = Input.GetAxisRaw("Mouse Y") * speed;
-                movementY = Input.GetAxisRaw("Mouse X") * speed;
-                MainCamera.transform.Translate(-movementY, -movementX, 0);
-
-
-
-        }
-
-	}
-}
-
 	void Update () { 
             if(Input.GetMouseButton(2))
                 Camera.main.transform.position += (new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"))) * speed;
