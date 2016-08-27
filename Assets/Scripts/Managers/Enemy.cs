281ea4dcfe801de0af4b2ccc97c4ace77757f52b
Houbar231 <matousfiala@gmail.com>
2016-08-27 08:46:46 +0200

    Added Better Camera Scroll And Enemy Spawn

---
+++ Assets/Scripts/Managers/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Enemy : MonoBehaviour {
+    public static Enemy Instance;
+
+    public List<MapTile> ValidSpawnTiles;
+    public List<EnemyUnit> EnemiesOnMap;
+
+    void Awake() {
+        Instance = this;
+        //ValidSpawnTiles = new List<MapTile>();
+        EnemiesOnMap = new List<EnemyUnit>();
+    }
+    void Start() {
+
+    }
+    public void SpawnEnemyUnit() {
+        MapTile SpawnTile = ValidSpawnTiles[Random.Range(0, ValidSpawnTiles.Count)];
+        EnemiesOnMap.Add(new EnemyUnit(SpawnTile.Data.x, SpawnTile.Data.y));
+    }
+}
+public class EnemyUnit {
+    public GameObject GO;
+    public EnemyData Data;
+
+    public EnemyUnit(int x, int y) {
+        GO = new GameObject("Enemy");
+        GO.transform.position = new Vector3(x, y, -0.001f);
+        GO.AddComponent<SpriteRenderer>().sprite = SpriteReference.r.Enemysprite;
+        Data = new EnemyData(x, y);
+    }
+}
+public class EnemyData {
+    public int x, y;
+    public EnemyData(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- Assets/Scripts/Managers/Map.cs
+++ Assets/Scripts/Managers/Map.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Map : MonoBehaviour {
     public static Map Instance;
@@ -14,5 +14,20 @@ public class Map : MonoBehaviour {
                 Tiles[i, j] = new MapTile(i, j);
             }
         }
+        Enemy.Instance.ValidSpawnTiles = GetEdgeTiles();
+        for(int i = 0; i < 4; i++) {
+            Enemy.Instance.SpawnEnemyUnit();
+        }
+    }
+    public List<MapTile> GetEdgeTiles() {
+        List<MapTile> ret = new List<MapTile>();
+        for(int i = 0; i < GeneralReference.r.Width; i++) {
+            for(int j = 0; j < GeneralReference.r.Height; j++) {
+                if(i == 0 || i == GeneralReference.r.Width - 1 || j == 0 || j == GeneralReference.r.Height-1) {
+                    ret.Add(Tiles[i, j]);
+                }
+            }
+        }
+        return ret;
     }
 }
