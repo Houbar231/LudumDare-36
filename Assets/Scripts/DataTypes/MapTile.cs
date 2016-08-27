c0aa8026f1cb894d204436aaa06c6977c59d42d3
Houbar231 <matousfiala@gmail.com>
2016-08-27 03:43:30 +0200

    Added basic tile structures

--- Assets/Scripts/DataTypes/MapTile.cs
+++ Assets/Scripts/DataTypes/MapTile.cs
@@ -2,11 +2,21 @@
 using System.Collections;
 
 public class MapTile {
-    public int x, y;
-    public bool IsWalkable;
+    public GameObject TileGO;
+    public TileData Data;
 
     public MapTile(int x, int y) {
+        Data = new TileData(x, y);
+        TileGO = new GameObject("Tile " + x + ", " + y);
+        TileGO.transform.position = new Vector3(x, y);
+        TileGO.AddComponent<SpriteRenderer>().sprite = SpriteReference.r.DefaultTileSprite;
+    }
+}
+public class TileData {
+    public int x, y;
+    public TileData(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
 }
