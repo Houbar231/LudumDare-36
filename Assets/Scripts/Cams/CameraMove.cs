34268104ebece4baf2b1f72b9bd5b8d401c9afbc
GLProgrammer <jirka2002@gmail.com>
2016-08-27 04:22:21 +0200

    Camera
    Camera Movement

---
+++ Assets/Scripts/Cams/CameraMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+    private Vector3 movement;
+    public Camera MainCamera;
+    public bool Arrows = true;
+    public float speed = 10.0f;
+    private float movementX, movementY;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Arrows == true)
+        {
+                movementX = Input.GetAxisRaw("Vertical") * speed;
+                movementY = Input.GetAxisRaw("Horizontal") * speed;
+                MainCamera.transform.Translate(movementY, movementX, 0);
+        }
+        else
+        {
+
+                movementX = Input.GetAxisRaw("Mouse Y") * speed;
+                movementY = Input.GetAxisRaw("Mouse X") * speed;
+                MainCamera.transform.Translate(-movementY, -movementX, 0);
+
+
+
+        }
+
+	}
+}
