97d5d2bc7572352368891db2fd22ae8988901cc3
GLProgrammer <jirka2002@gmail.com>
2016-08-28 05:34:50 +0200

    Small Update
    Small Uptade UI

--- Assets/Scripts/Building/BuildingButton.cs
+++ Assets/Scripts/Building/BuildingButton.cs
@@ -4,19 +4,11 @@ using System.Collections;
 
 public class BuildingButton : MonoBehaviour {
     public Button button;
+    public Sprite sprite;
     public GameObject Panel;
 	public void OnClick()
     {
-        switch (button.GetComponentInChildren<Text>().text)
-        {
-            case "Gold Mine":
-                Debug.Log("Gold Mine");
-                
-                break;
-            default:
-                Debug.Log("WTF??");
-                break;
-        }
+        
 
     }
 }
