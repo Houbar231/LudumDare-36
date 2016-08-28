40fa010ab26e8507ce93a3f09b29598a5afce5aa
GLProgrammer <jirka2002@gmail.com>
2016-08-28 11:37:40 +0200

    Revert "Added Building"
    This reverts commit 76d6df86cb8ec72dcb6e8a6f231ec28bff9aa9d6.
    Added Final Building

--- Assets/Scripts/Building/BuildingButton.cs
+++ Assets/Scripts/Building/BuildingButton.cs
@@ -1,14 +1,51 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
+using UnityEngine;
 using System.Collections;
 
-public class BuildingButton : MonoBehaviour {
-    public Button button;
-    public Sprite sprite;
-    public GameObject Panel;
-	public void OnClick()
+    public class BuildingButton : MonoBehaviour
     {
-        
+        public Button button;
+        public Sprite sprite;
+        public string bmode_Text;
+        public bool buildingMode = false;
+        public static BuildingButton Instance;
+        public Text Bmode_text;
+        public Button Bmode;
+        public string BuildingName;
+        public void Awake()
+        {
+            Instance = this;
+        }
 
+        public void Bmode_Click()
+        {
+            Debug.Log("Button Pressed");
+            HideOnStart.Instance.BHider.SetActive(false);
+            buildingMode = false;
+        }
+
+        public void OnClick()
+        {
+            BuildingShop.Instance.CloseShop();
+            buildingMode = true;
+
+            HideOnStart.Instance.BHider.SetActive(false);
+            Bmode_text.text = bmode_Text;
+            HideOnStart.Instance.BHider.SetActive(true);
+        }
+
+        void FixedUpdate()
+        {
+            if (Input.GetMouseButton(1) && buildingMode == true)
+            {
+                Vector3 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                int MouseX = Mathf.FloorToInt(MousePosition.x);
+                int MouseY = Mathf.FloorToInt(MousePosition.y);
+                MapTile SelectedTile = Map.Instance.Tiles[MouseX, MouseY];
+                GameObject GO = new GameObject(BuildingName);
+                GO.AddComponent<SpriteRenderer>().sprite = sprite;
+                GO.transform.position = new Vector3(SelectedTile.Data.x, SelectedTile.Data.y, 0);
+            }
+
+        }
     }
-}
---
+++ Assets/Scripts/GUI/HideOnStart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HideOnStart : MonoBehaviour
+{
+    public GameObject BHider;
+    public static HideOnStart Instance;
+    public Button Bmode;
+    public bool Visible;
+    void Awake()
+    {
+        Instance = this;
+        BHider.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
