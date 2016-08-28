76d6df86cb8ec72dcb6e8a6f231ec28bff9aa9d6
GLProgrammer <jirka2002@gmail.com>
2016-08-28 10:19:50 +0200

    Added Building
    U can build

--- Assets/Scripts/Building/BuildingButton.cs
+++ Assets/Scripts/Building/BuildingButton.cs
@@ -2,13 +2,50 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class BuildingButton : MonoBehaviour {
+public class BuildingButton : MonoBehaviour
+{
     public Button button;
     public Sprite sprite;
-    public GameObject Panel;
-	public void OnClick()
+    public string bmode_Text;
+    public bool buildingMode = false;
+    public static BuildingButton Instance;
+    public Text Bmode_text;
+    public Button Bmode;
+    public string BuildingName;
+    void Awake()
     {
+        Instance = this;
+    }
+    
+    public void Bmode_Click()
+    {
+        Debug.Log("Button Pressed");
+        HideOnStart.Instance.BHider.SetActive(false);
+        buildingMode = false;
+    }
+
+    public void OnClick()
+    {
+        BuildingShop.Instance.CloseShop();
+        buildingMode = true;
         
+        HideOnStart.Instance.BHider.SetActive(false);
+        Bmode_text.text = bmode_Text;
+        HideOnStart.Instance.BHider.SetActive(true);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(1)&& buildingMode == true)
+        {
+            Vector3 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            int MouseX = Mathf.FloorToInt(MousePosition.x);
+            int MouseY = Mathf.FloorToInt(MousePosition.y);
+            MapTile SelectedTile = Map.Instance.Tiles[MouseX, MouseY];
+            GameObject GO = new GameObject(BuildingName);
+            GO.AddComponent<SpriteRenderer>().sprite = sprite;
+            GO.transform.position = new Vector3(SelectedTile.Data.x, SelectedTile.Data.y, 0);
+        }
 
     }
 }
